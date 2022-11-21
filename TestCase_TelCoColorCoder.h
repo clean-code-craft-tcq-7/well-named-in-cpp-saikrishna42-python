4bc40ad4981ff33f93aeb6f4092bf18f9e714bd3
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-11-21 15:19:03 +0100

    Update TestCase_TelCoColorCoder.h
    comments added

--- TestCase_TelCoColorCoder.h
+++ TestCase_TelCoColorCoder.h
@@ -4,7 +4,7 @@
 
 using namespace TelCoColorCoder;
 
-
+//This function check based on given number correct major and minor color is choosen or not
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -16,13 +16,14 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.getMinor() == expectedMinor);
 }
 
+//This function check based on given major and minor color the correct pair number is choosen or not
 void testPairToNumber(
     TelCoColorCoder::MajorColor major,
     TelCoColorCoder::MinorColor minor,
     int expectedPairNumber)
 {
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    int get_pairNr_based_major_minor_clr = TelCoColorCoder::GetPairNumberFromColor(major, minor); //Getting the pair number
     std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
+    assert(get_pairNr_based_major_minor_clr == expectedPairNumber);
 }
 
